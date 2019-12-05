27ea019c98bbede094fa76c630e709643d879230
ClaraSalvadores <clara.salvadores@gmail.com>
2019-12-04 21:45:53 -0300

    estructura MVC

---
+++ app/controllers/mainController.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+// ************ Function to Read an HTML File ************
+function readHTML (fileName) {
+	let finalPath = path.join( __dirname, `../views/${fileName}.html`);
+	let htmlFile = fs.readFileSync(finalPath, 'utf-8');
+	return htmlFile;
+}
+
+const controller = {
+	root: (req, res) => {
+		let html = readHTML('index');
+		res.send(html);
+	},
+
+};
+
+module.exports = controller
+//let htmlFile = fs.readFileSync(path.join(__dirname, `/../views/${fileName}.html`), 'utf-8');
+//	return htmlFile;
+//}
+ 
