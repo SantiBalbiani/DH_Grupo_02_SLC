ffa31ceb4e61caea550a55f66fa9eb18ddae870f
SantiBalbiani <santiagobalbiani@hotmail.com>
2020-01-13 15:21:09 -0300

    elimino logica repetida

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -4,51 +4,6 @@ const m = require("../model/model");
 
 const fpath = path.join(__dirname, '../data/users.json');
 
-function getAllUsers () {
-	let usersFileContent = fs.readFileSync(usersFilePath, 'utf-8');
-	let users;
-	if (usersFileContent == '') {
-		users = [];
-	} else {
-		users = JSON.parse(usersFileContent);
-	}
-	return users;
-};
-
-function generateUserId () {
-	let users = getAllUsers();
-	if (users.length == 0) {
-		return 1;
-	}
-	let lastUser = users.pop();
-	return lastUser.userId + 1;
-}
-function guardarUser (newUser) {
-let users = getAllUsers();
-	users.push(newUser);
-    fs.writeFileSync(usersFilePath, JSON.stringify(users, null, ' '));
-};
-
-function borrarUser(userId){
-	let users = getAllUsers();
-	users = users.filter( usr => usr.userId != userId);
-	fs.writeFileSync(usersFilePath, JSON.stringify(users, null, ' '));
-}
-
-function guardarCambiosUser(user) {
-
-	let usersContent = fs.readFileSync(usersFilePath, 'utf-8');
-	let users;
-	if (usersContent == '') {
-		users = [];
-	} else {
-		users = JSON.parse(usersContent);
-	}
-	users = users.filter( usr => usr.id != user.id);
-	users.push(user);
-	fs.writeFileSync(usersFilePath, JSON.stringify(users, null, ' '));
-	};
-
     const controller = {
         getUsers: (req, res) => {
             let allUsers = m.loadFile(fpath); 
@@ -83,24 +38,12 @@ function guardarCambiosUser(user) {
         },
         saveChanges: (req, res) => {
         //	let html = readHTML('productAdd2');
-    
-
-
-        let allUsers = m.loadFile(fpath);
-        
         let user = {
 			id: parseInt(req.params.id),
 			...req.body,
 			image: req.file.filename,
 		};
 		m.saveChanges(user, fpath);
-        /* 
-        elUser = allUsers.find( usr => usr.userId == req.params.userId )
-        elUser.nombre = req.body.nombre;
-        elUser.apellido = req.body.apellido;
-        elUser.email= req.body.email;
-        elUser.image = req.file.filename,
-        guardarCambiosUser(elUser); */
         let guardado = "/users/" + user.id + "/edit" 
         res.redirect(guardado);
         },
@@ -111,3 +54,4 @@ function guardarCambiosUser(user) {
     };
     
     module.exports = controller
+
