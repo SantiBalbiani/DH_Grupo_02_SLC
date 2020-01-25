1cad3ef3c319533826ef67ca1ea94f257b608b72
SantiBalbiani <santiagobalbiani@yahoo.com>
2020-01-25 14:02:35 -0300

    fix menor vista

--- app/middlewares/userCookie.js
+++ app/middlewares/userCookie.js
@@ -6,16 +6,12 @@ const bcrypt = require('bcryptjs');
 
 function userCookie (req, res, next) {
     if (req.cookies.user != undefined){
-        // req.cookies.user está hasheado. Compararlo con todos los id de users
         var finded = true;
         data = model.loadFile(userFilePath);
         data.forEach( user => {
-            
             finded = bcrypt.compareSync( user.id.toString(), req.cookies.user.toString()  );
-            
             if (finded){
                 req.session.user = model.find_("id", user.id, userFilePath);
-                
                 next();
                 }
         } );
