822f2a0351ec2cbfa0c25b3cd7c0e1da8f86de19
SantiBalbiani <santiagobalbiani@hotmail.com>
2020-03-29 21:02:05 -0300

    vuelvo dinamicos los componentes

--- app/dashboard/src/App.js
+++ app/dashboard/src/App.js
@@ -6,20 +6,14 @@ import Indicators from './components/Indicators';
 import LineGraph from './components/LineGraph';
 import NavBar from './components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import axios from 'axios';
 import API from './config/API';
-/* axios.get('http://localhost:3030/api/products/1')
-.then(res => {
-  const productos = res.data;
-  this.setState({ products: productos });
-}) */
 
 class App extends React.Component {
   
   constructor(props){
     super(props);
     this.state = {
-
+      categories: [],
     }
   }
 
@@ -30,23 +24,10 @@ class App extends React.Component {
   return (
       <React.Fragment>
 			<NavBar/>
-     
-      
       <section className="container">
       <div className="row">
-          <div className="col-xl-4 col-lg-6 col-md-6 col-sm-6 col-12">
-          <Indicators
-          img = {require(`${images.categoryLogo}`)} title= {`Cantidad de Productos de Categoría ` }
-           />
-          </div>
-          <div className="col-xl-4 col-lg-6 col-md-6 col-sm-6 col-12">
-          <Indicators
-          img = {categoryIMG} title="Cantidad de Productos de Categoría" />
-          </div>
-          <div className="col-xl-4 col-lg-6 col-md-6 col-sm-6 col-12">
-          <Indicators
-          img = {logo} title="Cantidad de Productos de Categoría" />
-          
+      {this.state.categories.map( (cat, idx) => 
+      <div className="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-12"> <Indicators img = {categoryIMG} title = {cat.categoryName} /> </div>)}
         </div>
         <div className="row">
           <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
@@ -54,31 +35,21 @@ class App extends React.Component {
           </div>
         </div>
 
-     </div>
+     
      </section>
      </React.Fragment>
     
   )
 }
 async componentDidMount(){
-  let userData = await API.get('products/allCategories');
+  let allCategories = await API.get('products/allCategories');
 
-      // Parse the results for ease of use.
-  userData = userData.data;
-  console.log(userData);
-  // Update state with new data and re-render our component.
-/*   const name = `${userData.name.first} ${userData.name.last}`;
-  const avatar = userData.picture.large;
-  const email = userData.email;
+  allCategories = allCategories.data;
+  console.log(allCategories);
 
   this.setState({
-    ...this.state, ...{
-      isLoading: false,
-      name,
-      avatar,
-      email
-    }
-  }); */
+    categories: allCategories,
+  }); 
 }
 
 }
