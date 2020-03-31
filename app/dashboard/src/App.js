6dad5d6c1d57ab21032bf705d309e6d5a88764e1
SantiBalbiani <santiagobalbiani@hotmail.com>
2020-03-31 00:54:16 -0300

    Agrego control de grafico

--- app/dashboard/src/App.js
+++ app/dashboard/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 /* import logo from './logo.svg'; */
 import categoryIMG from './images/category2.jpg';
 import './App.css';
@@ -7,61 +7,60 @@ import LineGraph from './components/LineGraph';
 import NavBar from './components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import API from './config/API';
+import 'react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css';
+import RangeSlider from 'react-bootstrap-range-slider';
 
 class App extends React.Component {
   
   constructor(props){
     super(props);
     this.handler = this.handler.bind(this);
+    this.updGraph = this.updGraph.bind(this);
+    
     this.state = {
-      categories: [],
-      idCat_graphic: 1,
-      label: 'new Arduino Products'
+      data: [],
+      id_graphic: 1,
+      label: ' ',
+      value: 4,
     }
   }
 
-  handler(idCategory, name){
+  handler(id, name){
     this.setState(
-      { idCat_graphic: idCategory,
+      { id_graphic: id,
       label: `New ${name} products`, }
     );
-    
-
-  /*   if (this.state.idCat_graphic !== idCategory){
-      this.setState(
-        { idCat_graphic: 1 }
-      );
-      console.log(this.state.idCat_graphic);
-    } */
-    
-    
   }
 
-
+  updGraph(axis_x){
+    this.setState(
+      { timeLength: axis_x }
+    );
+  }
 
   render(){
-/*     const images = {
-      categoryLogo : './images/category.png',
-    }; */
+   
   return (
       <React.Fragment>
-			
       <section className="container">
-       
       <div className="row">
         <div className="col-3" >
             <NavBar/>
         </div>
         <div className="col-9">
-
         <div className="row">
-            {this.state.categories.map( (cat, idx) => 
+            {this.state.data.map( (cat, idx) => 
              <Indicators key={idx} img={categoryIMG} title={cat.categoryName} idCategory={cat.id} handler={this.handler.bind(this)} />)}
         </div>
         <div className="row">
-          <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-              <LineGraph key={Math.random()} param = {this.state.idCat_graphic} months = '4' label= {this.state.label} />
+          <div className="col-xl-10 col-lg-12 col-md-12 col-sm-12 col-12">
+              <LineGraph key={Math.random()} param = {this.state.id_graphic} months = {this.state.value} label= {this.state.label} />
 
+          </div>
+          <div className="col-xl-2 col-lg-12 col-md-12 col-sm-12 col-12">
+           <div className="text"> Choose the Time Length [Months] </div>
+           <RangeSlider value={this.state.value} min="1" max="12" step="1" onChange={ event => this.setState( { value: event.target.value })}  /> 
+          
           </div>
         </div>
           </div>
@@ -74,7 +73,7 @@ async componentDidMount(){
   let allCategories = await API.get('products/allCategories');
   allCategories = allCategories.data;
   this.setState({
-    categories: allCategories,
+    data: allCategories,
   }); 
 
 }
